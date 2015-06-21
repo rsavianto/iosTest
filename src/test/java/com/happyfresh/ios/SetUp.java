cbb556e6961b92d4ddb89a47ca4201e119a2facd
Ronald Savianto <rsavianto@yahoo.com>
2015-06-21 13:20:42 +0700

    Initial commit

---
+++ src/test/java/com/happyfresh/ios/SetUp.java
@@ -0,0 +1,36 @@
+package com.happyfresh.ios;
+
+import java.io.File;
+import java.net.URL;
+
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.ios.IOSDriver;
+
+public class SetUp {
+	
+	IOSDriver driver;
+	
+	@BeforeClass
+    public void setUp() throws Exception {
+//        File classpathRoot = new File(System.getProperty("user.dir"));
+        File appDir = new File("/Users/rsavianto/Projects/Makana/ios/test");
+        File app = new File(appDir, "HappyFresh.app.zip");
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("appium-version","1.4.0");
+        capabilities.setCapability("deviceName","iPhone Simulator");
+        capabilities.setCapability("platformVersion", "7.1");
+        capabilities.setCapability("platformName", "ios");
+        capabilities.setCapability("app", app.getAbsolutePath());
+        driver = new IOSDriver(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
+    }
+
+    @AfterClass
+    public void tearDown() {
+        driver.quit();
+    }
+
+}
