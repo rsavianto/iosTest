89a78f6f12156fab853f118dcf74ae716c5d68b1
Ronald Savianto <rsavianto@yahoo.com>
2015-06-30 12:48:54 +0700

    Read capabilities from env

--- src/test/java/com/happyfresh/ios/SetUp.java
+++ src/test/java/com/happyfresh/ios/SetUp.java
@@ -16,18 +16,16 @@ public class SetUp {
 	
 	@BeforeClass
     public void setUp() throws Exception {
-//        File classpathRoot = new File(System.getProperty("user.dir"));
+		String appPath = System.getenv("IOS_APP_PATH");
+		String appName = System.getenv("IOS_APP_NAME");
+		String deviceName = System.getenv("IOS_DEVICE_NAME");
+		String platformVersion = System.getenv("IOS_PLATFORM_VERSION");
 
-		File appDir = new File("./");
-
-        //File appDir = new File("/Users/mohadzanegisetigung/Documents/ios/");
-
-        File app = new File(appDir, "HappyFresh.app.zip");
+		File appDir = new File(appPath);
+        File app = new File(appDir, appName);
         DesiredCapabilities capabilities = new DesiredCapabilities();
-        capabilities.setCapability("appium-version","1.4.0");
-        capabilities.setCapability("deviceName","iPhone Simulator");
-        capabilities.setCapability("platformVersion", "8.3");
-        capabilities.setCapability("platformName", "ios");
+        capabilities.setCapability("deviceName", deviceName);
+        capabilities.setCapability("platformVersion", platformVersion);
         capabilities.setCapability("app", app.getAbsolutePath());
         driver = new IOSDriver(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
     }
